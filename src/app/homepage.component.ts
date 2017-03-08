c229305463922c85e33b494ca799a830d57de10f
Bradley Campbell <bb.campbell606@gmail.com>
2017-03-08 12:59:20 -0700

    initial commit

---
+++ src/app/homepage.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-app',
+  template: `<h1>Project {{name}}</h1>`,
+})
+export class HomepageComponent  { name = 'Freelance'; }
